56d4e999ae583aca8192419a2cb2c7252785ead2
Riley Kacmarynski <riley.kacmarynski@gmail.com>
2023-12-01 12:38:10 -0600

    day 1

---
+++ day1/day1.ts
@@ -0,0 +1,35 @@
+import { readFileSync } from 'fs'
+
+const numberWords = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+} as const
+
+const input = readFileSync('./day1-input.txt', 'utf-8')
+  .split('\n')
+  .map((line) =>
+    Array.from(line).reduce((numbers, char, index) => {
+      if (!Number.isNaN(Number(char))) {
+        return [numbers[0] ?? Number(char), Number(char)]
+      } else {
+        const word = line.slice(Number(index), line.length + 1)
+        for (const [key, value] of Object.entries(numberWords)) {
+          if (word.indexOf(key) === 0) {
+            return [numbers[0] ?? value, value]
+          }
+        }
+      }
+
+      return numbers
+    }, [] as number[])
+  )
+  .reduce((sum, next) => sum + parseInt(next.join(''), 10), 0)
+
+console.log(input)
